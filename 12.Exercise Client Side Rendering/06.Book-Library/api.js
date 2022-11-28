a359e9a9e2a77d3c06ad25899e9590558a0d42c9
Deniz <d.ivan0v.6100@gmail.com>
2022-11-28 21:57:27 +0200

    added some exercises solutions

---
+++ 12.Exercise Client Side Rendering/06.Book-Library/api.js
@@ -0,0 +1,31 @@
+const baseUrl = 'http://localhost:3030/jsonstore/collections/books'
+
+export async function get(){
+    let response = await fetch(baseUrl);
+    let result = await response.json();
+
+    return [...Object.entries(result)];
+}
+
+export async function post({author, title}){
+    return await fetch(baseUrl, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({author, title})
+    });
+}
+
+export async function put(id, data){
+    return await fetch(`${baseUrl}/${id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+
+}
+
+export async function del(id){
+    return await fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE'
+    });
+}
